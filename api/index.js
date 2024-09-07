f408061fa281fa9ed95191ec841b71b18b83df6d
rishabhag18 <18r.agarwal@gmail.com>
2024-09-07 23:22:11 +0530

    database added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app= express();
+dotenv.config();
+//const mongoURI= "mongodb+srv://shaibalpatra7:dRoaxvDP1MBsQrHK@cluster0.wrc1vh3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Database Connected");
+}).catch((err)=>{
+    console.log(err);
+})
+const app = express();
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log(`Server is running at port 3000.`);
 })
