7fb355f51e8ac09a878c435d29873446510a9083
rishabhag18 <18r.agarwal@gmail.com>
2024-09-09 00:03:16 +0530

    signin page client side functionality added and redux toolkit used for global state management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+  ,
 )
