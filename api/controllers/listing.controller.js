4699880d2cb904173a5f78a3413f0b2b5ff10554
rishabhag18 <18r.agarwal@gmail.com>
2024-11-01 13:16:09 +0530

    delete user functionality resolved

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -64,7 +64,7 @@ export const getListing = async(req,res,next) => {
     }
 };
 
-//For the search button
+//For the search buttonn
 export const getListings = async(req,res,next) => {
     try {
         const limit = parseInt(req.query.limit) || 9;
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -40,7 +40,9 @@ export const updateUser = async (req, res, next) => {
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, "You can only update your own account!"));
+  
   try {
+    await Listing.deleteMany({userRef: `${req.user.id}`});
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token");
     res.status(200).json("User has been deleted");
